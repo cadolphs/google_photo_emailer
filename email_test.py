09cf99a79b6033fb93013d69271e3c0bf2e85b88
Clemens Adolphs <clemens.adolphs@1qbit.com>
2023-08-22 09:16:10 -0700

    Moving auth stuff to own module

--- email_test.py
+++ email_test.py
@@ -1,13 +1,12 @@
-import json
-
 import sys
 
-from photo_emailer.email_client import EmailClient, AuthClient
+from photo_emailer.email_client import EmailClient
+from photo_emailer.authentication import AuthClient
 
 auth_client = AuthClient.create()
 creds = auth_client.get_credentials()
 
-client = EmailClient.create(credentials=creds)
+client = EmailClient.create_null(credentials=creds)
 client.send_email(to="clemens.adolphs+test@gmail.com", subject="test", body="Hello!")
 
 sys.exit(0)
--- photo_emailer/email_client.py
+++ photo_emailer/email_client.py
@@ -1,10 +1,7 @@
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from requests import HTTPError
 from email.mime.text import MIMEText
 import base64
-from google.oauth2.credentials import Credentials
-from google.auth.transport.requests import Request
 
 
 class EmailClient:
@@ -21,67 +18,6 @@ class EmailClient:
 
     def send_email(self, to, subject, body):
         self.api.send_email(to, subject, body)
-
-
-class AuthClient:
-    def __init__(self, auth_api=None):
-        self.api = auth_api()
-
-    @classmethod
-    def create(cls):
-        return cls(GoogleAuthAPI)
-
-    @classmethod
-    def create_null(cls):
-        return cls(AuthStub)
-
-    def get_credentials(self):
-        return self.api.get_credentials()
-
-
-class GoogleAuthAPI:
-    def run_browser_authentication(self):
-        SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
-        flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-        return flow.run_local_server(port=0)
-
-    def get_credentials(self):
-        stored_creds = self.load_credentials("token.json")
-
-        if stored_creds and stored_creds.expired and stored_creds.refresh_token:
-            print("Refreshing expired credentials...")
-            stored_creds.refresh(Request())
-            self.store_credentials(stored_creds)
-            return stored_creds
-        elif not stored_creds.expired:
-            print("Reusing stored credentials...")
-            return stored_creds
-        else:
-            print("No reusable credentials found; authenticating again...")
-            creds = self.run_browser_authentication()
-            self.store_credentials(creds)
-            return creds
-
-    def store_credentials(self, creds):
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
-
-    def load_credentials(self, json):
-        return Credentials.from_authorized_user_file(json)
-
-
-class AuthStub:
-    def get_credentials(self):
-        creds = {
-            "token": "TOKEN",
-            "refresh_token": "REFRESH TOKEN",
-            "token_uri": "https://oauth2.googleapis.com/token",
-            "client_id": "CLIENTID",
-            "client_secret": "CLIENTSECRET",
-            "scopes": ["https://www.googleapis.com/auth/gmail.send"],
-            "expiry": "EXPIRY",
-        }
-        return creds
 
 
 class GmailSendClient:
